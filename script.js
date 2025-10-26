267e201ec70e95e098a0b15ccc4af4ee254a43f6
Diego Armando Ustariz Mejia <dustariz@bancolombia.com.co>
2025-10-26 16:44:02 -0500

    Sitio Web JoinData v2

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const menuToggle = document.getElementById("menu-toggle");
+const navMenu = document.querySelector(".nav-menu ul");
+
 function scrollToSection(id) {
   document.getElementById(id).scrollIntoView({ behavior: "smooth" });
 }
@@ -6,4 +9,8 @@ document.querySelector("form")?.addEventListener("submit", (e) => {
   e.preventDefault();
   alert("Gracias por contactarnos. Pronto te responderemos.");
   e.target.reset();
+});
+
+menuToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("show");
 });
